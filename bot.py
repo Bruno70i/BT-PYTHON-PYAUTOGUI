16bc665681e996620a127c3f1a7a9cd5af8554d1
bruno70i <bruno.npsilva70@gmail.com>
2025-04-27 22:39:55 -0300

    Testes

--- bot.py
+++ bot.py
@@ -9,23 +9,17 @@ import time
 
 
 pyautogui.press("win")
-time.sleep(3)
+time.sleep(1)
 
 pyautogui.write("google")
-time.sleep(3)
+time.sleep(1)
 
 pyautogui.press("enter")
-time.sleep(3)
+time.sleep(1)
 
 
-# Clica no link do instagram
-pyautogui.click(x=430, y=90)
-time.sleep(5)
-
-
-
-# Clica no comentario do instagram
-posicao = pyautogui.locateOnScreen("logo_comentario.png", confidence=0.8)
+# Clica no link com logo do instagram
+posicao = pyautogui.locateOnScreen("insta-link-logo.png", confidence=0.8)
 
 if posicao:
     # Obtenha as coordenadas do centro da imagem
@@ -37,6 +31,55 @@ else:
     print("Imagem não encontrada.")
 
 time.sleep(4)
+
+
+
+
+# Clica na pagina inicial para ativar o cursos scroll
+posicao = pyautogui.locateOnScreen("pagina-insta-inicial.png", confidence=0.8)
+
+if posicao:
+    # Obtenha as coordenadas do centro da imagem
+    centro = pyautogui.center(posicao)
+    # Clique no centro da imagem
+    pyautogui.click(centro)
+    print("Imagem encontrada e clicada!")
+else:
+    print("Imagem não encontrada.")
+
+time.sleep(2)
+
+
+# Clica no comentario do instagram
+imagem_encontrada = False
+
+# Define um número máximo de tentativas (opcional)
+max_tentativas = 30
+tentativas = 0
+
+while not imagem_encontrada:
+    try:
+        # Tentar localizar a imagem na tela
+        posicao = pyautogui.locateOnScreen("logo_comentario.png", confidence=0.8)
+        
+        # Caso a imagem seja encontrada, clique nela e saia do loop
+        centro = pyautogui.center(posicao)
+        pyautogui.click(centro)
+        print("Imagem encontrada e clicada!")
+        imagem_encontrada = True
+    
+    except pyautogui.ImageNotFoundException:
+        # Caso a imagem não seja encontrada, role a tela para baixo e continue
+        print("Rolando a tela até encontrar....")
+        pyautogui.scroll(-100)
+        time.sleep(3)
+        
+        # Incrementar contador de tentativas (opcional)
+        tentativas += 1
+        if max_tentativas > 0 and tentativas >= max_tentativas:
+            print(f"Desistindo após {max_tentativas} tentativas")
+            break
+
 
 
 
